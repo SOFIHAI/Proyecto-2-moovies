b063d3140926ef3d14adee4c500c964bf992bf8f
Sofia Haiquel <sofihaiquel@gmail.com>
2023-09-17 20:16:30 -0300

    ägrego cambios en pag de detalles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import myFooter from '../components/footer.js';
 import navbar from '../components/navbar.js';
 import { obtenerInformacion } from "./utils.js";
+import { cargarPeliculas } from './detalles.js';
+
 
 
 
@@ -17,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 let contenedor = document.getElementById("contendorPeliculas");
 
 
+
 let infoPeliculas = await obtenerInformacion();
 
 
@@ -26,12 +29,12 @@ infoPeliculas?.peliculas?.map(pelicula => {
   div.className = "col-lg-4 col-xl-4 col-md-4 col-3 mt-3 p-1 cards-principal" ;
   div.id = "cards-1";
   div.innerHTML = `
-  <div class="contenedorImgPelicula">
+  <div class="contenedorImgPelicula id="detales">
   <img src="${pelicula.img}" alt="" class="h-100 w-100">
   
 </div>
    <h5 class="mt-5 text-white">${pelicula.titulo}</h5>
-   <button href="../detalle.html" class="detalle-btn btn btn-outline-dark btn-sm me-4 mb-2 fs-6 text-white" id="">Detalles</button>
+   <button href="../detalle.html" class="detalle-btn btn btn-outline-dark btn-sm me-4 mb-2 fs-6 text-white" id="btn-detalle" onclick="${async()=>cargarPeliculas("${pelicula.id}")}">Detalles</button>
    
   `; 
   contenedor.appendChild(div);
